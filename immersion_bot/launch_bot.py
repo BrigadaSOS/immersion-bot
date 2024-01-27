c67881187e71fdbd1c2e58a42a566d5b573b994b
David Afonso <davafons@gmail.com>
2024-01-28 00:09:25 +0900

    Remove setlocale

--- immersion_bot/launch_bot.py
+++ immersion_bot/launch_bot.py
@@ -3,7 +3,6 @@ import logging
 import os
 import sys
 import traceback
-import locale
 
 import discord
 from discord import AppInfo
@@ -47,7 +46,6 @@ class MyBot(commands.Bot):
             intents=discord.Intents.all(),
             tree_cls=CustomCommandTree,
         )
-        locale.setlocale(locale.LC_TIME, "es_ES.UTF-8")
 
     async def on_error(self, event_method: str, /, *args, **kwargs):
         log.exception("Ignoring exception in %s", event_method)
